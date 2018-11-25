5e71a9cdd46463fa9ed6a31790e4118343ed4505
mbax9kp5 <kyriakos.pieris@student.manchester.ac.uk>
2018-11-25 20:40:20 +0000

    Initial Setup

---
+++ src/main/java/com/nodal/twittersphere/twittersphere/TwittersphereApplication.java
@@ -0,0 +1,12 @@
+package com.nodal.twittersphere.twittersphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittersphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittersphereApplication.class, args);
+	}
+}
